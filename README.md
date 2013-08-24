68027fb8dae6163221a8cda4a57b64c8b91f962c
Manuel Pita <mpita1984@gmail.com>
2013-08-24 04:53:24 +0000

    Creacion del proyecto en Django 1.5.2

---
+++ README.md
@@ -0,0 +1,4 @@
+Generar PDF
+===========
+
+Prueba para genrar PDF utilizando Django 1.5.2, Python 2.7.3 y reportlab
---
+++ generar_pdf/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'generar_pdf.views.home', name='home'),
+    # url(r'^generar_pdf/', include('generar_pdf.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
